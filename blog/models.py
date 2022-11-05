9e153c736a699161ee1ac3b33c6474c6646bb9c6
VitorAntonioKuhnen <vitorkuhnen14@gmail.com>
2022-11-05 14:31:40 -0300

    Ajustado a criação de publicação

--- blog/models.py
+++ blog/models.py
@@ -16,16 +16,16 @@ class Publicacao(models.Model):
     resumo = models.TextField(max_length=1000)
     conteudo = models.TextField()
     visualizacoes = models.IntegerField()
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.titulo
 
 class Comentarios(models.Model):
     comentario = models.TextField()
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    publicacao = models.ForeignKey(Publicacao, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    publicacao = models.ForeignKey(Publicacao, on_delete=models.DO_NOTHING)
     datHora = models.DateTimeField()
     
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from asyncio.windows_events import NULL
 from django.shortcuts import render, redirect
 from django.contrib import auth, messages
+from .models import Categoria
+from django.contrib.auth.models import User
 
 from blog.models import Publicacao
 
@@ -8,33 +10,52 @@ from blog.models import Publicacao
 def inicio(request):
     return render(request)
 
+
 def cadastroPub(request):
-    return render(request, 'CadPubli.html') 
+    return render(request, 'CadPubli.html')
+
 
 def publicacao(request):
-    return render(request, 'publicacao.html')   
+    return render(request, 'publicacao.html')
+
 
 def criaPub(request):
-    titulo = request.POST.get('titulo')
-    resumo = request.POST.get('resumo') 
-    conteudo = request.POST.get('conteudo') 
-    imagem = Publicacao.objects.all()
-    imagem = request.FILES.get('imagem')
-    categoria = request.POST.get('')
+    contexto = {"categorias": Categoria.objects.all()}
+
+
+    if request.method == "POST":
+        titulo = request.POST.get('titulo')
+        resumo = request.POST.get('resumo')
+        conteudo = request.POST.get('conteudo')
+        # imagem = Publicacao.objects.all()
+        imagem = request.FILES.get('imagem')
+        idCategoria = Categoria.objects.get(id=request.POST.get('tipCateg'))
     
-    if request == "POST":
+
         if titulo is NULL:
-            messages.error(request,"Informe um Titulo!")
-        
+            messages.error(request, "Informe um Titulo!")
+
         elif resumo is NULL:
-            messages.error(request,"Adicione um Resumo")
+            messages.error(request, "Adicione um Resumo")
 
         elif conteudo is NULL:
-            messages.error(request,"É necessario adicionar o conteudo para criar uma publicação")
+            messages.error(
+                request, "É necessario adicionar o conteudo para criar uma publicação")
 
         else:
-            Publicacao.objects.create(titulo = titulo, resumo=resumo, conteudo=conteudo, endImage=imagem, visualizacoes=0, usuario_id=request.user.id, categoria=categoria)
-            return redirect(request, 'inicio')
-        
+            print("Titulo: " + titulo)
+            print("Resumo:" + resumo)
+            print("Conteudo:" + conteudo)
+            print(imagem)
+            # print(categoria)
+
+            Publicacao.objects.create(titulo=titulo, resumo=resumo, conteudo=conteudo, endImage=imagem, visualizacoes=0, usuario_id=request.user.id, categoria=idCategoria)
+            return redirect('publicacao')
+
     else:
-        return render(request, 'publicacao.html')    
+    #     print("Titulo:" + titulo)
+    #     print("Resumo:" + resumo)
+    #     print("Conteudo:" + conteudo)
+    #     print("Imagem:" + imagem)
+    #     print("Categoria:" + categoria)
+        return render(request, 'CadPubli.html', contexto)
